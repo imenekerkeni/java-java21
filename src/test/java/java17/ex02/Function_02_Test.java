da28fffd8a91c7b1cbbace85e3c1d176442c798d
IMENE KERKENI <imene1995kerkeni@gmail.com>
2026-07-31 15:28:09 +0200

    Exercices Function 01 et Function 02 complétés

--- src/test/java/java17/ex02/Function_02_Test.java
+++ src/test/java/java17/ex02/Function_02_Test.java
@@ -15,14 +15,17 @@ public class Function_02_Test {
     //  tag::buildAccount[]
     // TODO Compléter la fonction buildAccount
     // TODO la fonction possède 2 paramètres en entrée : une personne et un solde
-    BiFunction<Person, Integer, Account> buildAccount = null;
+    BiFunction<Person, Integer, Account> buildAccount = (p, balance) -> new Account(p, balance);
     //  end::buildAccount[]
 
     @Test
     public void test_build_account() throws Exception {
 
         // TODO invoquer la fonction buildAccount pour que le test soit passant
-        Account account = null;
+        Account account = buildAccount.apply(
+                new Person("John", "France", 80, "pass"),
+                500
+        );
 
         assert account.getBalance().equals(500);
         assert account.getOwner().getFirstname().equals("John");
@@ -30,6 +33,4 @@ public class Function_02_Test {
         assert account.getOwner().getAge().equals(80);
         assert account.getOwner().getPassword().equals("pass");
     }
-
-
 }
